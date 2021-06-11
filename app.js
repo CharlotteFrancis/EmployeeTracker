785660dfad29043a9e0ef4bc6fd5f43eac7cb4b6
Charlotte Wong <ChiharuTaru@gmail.com>
2021-06-11 14:40:47 -0700

    added view function

---
+++ app.js
@@ -0,0 +1,90 @@
+const inquirer = require('inquirer')
+const mysql = require('mysql2')
+const db = mysql.createConnection('mysql://root:rootroot@localhost:3306/employees_db')
+require('console.table')
+
+// ask again
+
+// make some dummy data
+
+// view function
+const view = _ => {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'action',
+      choices: ['All Employees', 'Employees By Department', 'Employees by Manager'],
+      message: 'What would you like to view?'
+    }
+  ])
+    .then(({ action }) => {
+      switch (action) {
+        case 'All Employees':
+          db.query('SELECT * FROM employees', (err, employees) => {
+            if (err) {
+              console.log(err)
+            }
+            console.table(employees)
+          })
+          break
+        case 'Employees By Department':
+          // potentially another query where they select by dpt.
+          break
+        case 'Employees by Manager':
+          // query where they select by manager
+          break
+      }
+    })
+    .catch(err => console.log(err))
+}
+
+// view budget of a dpt
+
+// add function
+const add = _ => {
+  // a
+}
+
+// remove function
+const rm = _ => {
+  // a
+}
+
+// update employee role
+// update employee manager
+const update = _ => {
+  // a
+}
+
+// ask
+// prompt to add, view, or update
+const ask = _ => {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'action',
+      choices: ['View Employees', 'Add Employee', 'Remove', 'Update Employee']
+    }
+  ])
+    .then(({ action }) => {
+      switch (action) {
+        case 'View Employees':
+          view()
+          break
+        case 'Add Employee':
+          add()
+          break
+        case 'Remove':
+          rm()
+          break
+        case 'Update Employee':
+          update()
+          break
+        default:
+          console.log('error in action switch in ask function')
+          break
+      }
+    })
+    .catch(err => console.log(err))
+}
+ask()
